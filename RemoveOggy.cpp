1c0938009bf2db9009ed4b5cc9bef1155bc49eb9
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-08-13 19:30:15 +0530

    Add files via upload
    Added the CPP file from my system.

---
+++ RemoveOggy.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int total=5;
+int main()
+{
+    string names[5]={ "nobita", "bheem", "oggy", "oggy cockroach", "ninja" };
+
+    removeOggy(names);
+    if(OggyIsRemoved(names))
+    {
+        cout<< "Passed!" << endl;
+        return 0;
+    }
+    else
+    {
+        cout<< "Failed!" << endl;
+        return 1;
+    }
+}
+
+void removeOggy(string names[5])
+{
+    int name;
+    string base = "oggy";
+    int len = base.size();
+    int pointer;
+    int removepointer;
+
+
+    for(name=4;name>=0;name--)
+    {
+        if(names[name].size()>= len)
+        {
+           if((base.compare((names[name]).substr(0,len)))==0)
+           {
+               pointer=name;
+               for(removepointer=pointer;removepointer<total-1;removepointer++)
+               {
+                   names[removepointer]= names[removepointer+1];
+               }
+
+               total=total-1;
+           }
+        }
+    }
+}
+
+bool OggyIsRemoved(string names[total])
+{
+    int name;
+    int flag=0;
+    string base = "oggy";
+    int len = base.size();
+    for(name=0;name<total;name++)
+    {
+        if((base.compare((names[name]).substr(0,len)))==0)
+        {
+            flag=1;
+            break;
+        }
+
+    }
+
+    if(flag==1)
+        return false;
+    else
+        return true;
+
+}
